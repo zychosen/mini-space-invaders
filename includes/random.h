5e1fc77255a28b636c8aa6d9b7efae0850358049
zychosen <npavan99@gmail.com>
2021-09-15 17:20:44 +0530

    random number generator (16-bit) header file

---
+++ includes/random.h
@@ -0,0 +1,18 @@
+/*
+ * random
+ *
+ *  Created on: Sep 10, 2021
+ *      Author: zychosen
+ */
+
+#ifndef RANDOM_H_
+#define RANDOM_H_
+
+#define a 49381
+#define c 8643
+#define m 65536
+
+void Seed(unsigned int);
+unsigned int random(void);
+
+#endif /* RANDOM_H_ */
